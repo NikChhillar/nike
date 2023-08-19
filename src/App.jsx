4c81cd7075c0883614c6524c627aa88b6167bf2d
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-08-19 10:21:46 +0530

    initial commit -setup tailwind css + vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+    <section className="padding">Popular Products</section>
+    <section className="padding">Super quality</section>
+    <section className="padding-x py-10">Services</section>
+    <section className="padding">Special offer</section>
+    <section className="padding bg-pale-blue">Customer reviews</section>
+    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+    <section className="padding-x padding-t pb-8 bg-black">footer</section>
+  </main>
+);
+
+export default App;
